af6ad94fbdfdcfc1f0208d93ea8d51279c34fdb0
Tyler K <tylerkruer1@gmail.com>
2023-12-11 19:31:29 -0700

    init commit - will add finalized details + deployment after interview

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { LeetcodeQuestion } from "./src/types"
 import { sendSMS } from './src/sms';
 
 async function main() {
-    const leetcodeQuestions = JSON.parse(fs.readFileSync('./problems.json', 'utf8')) as LeetcodeQuestion[];
+    const leetcodeQuestions = JSON.parse(fs.readFileSync('./src/problems.json', 'utf8')) as LeetcodeQuestion[];
     const randomQuestion = leetcodeQuestions[Math.floor(Math.random() * leetcodeQuestions.length)];
     const randomQuestionTitle = randomQuestion.stat.question__title;
     const randomQuestionTitleSlug = randomQuestion.stat.question__title_slug;
